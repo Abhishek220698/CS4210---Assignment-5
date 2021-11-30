185039c3527f793c38ccea537a54ec6203e6b7fc
Abhishek220698 <91398488+Abhishek220698@users.noreply.github.com>
2021-11-29 23:24:14 -0800

    Add files via upload

---
+++ association_rule_mining.py
@@ -0,0 +1,105 @@
+#-------------------------------------------------------------------------
+# AUTHOR: your name
+# FILENAME: title of the source file
+# SPECIFICATION: description of the program
+# FOR: CS 4200- Assignment #5
+# TIME SPENT: how long it took you to complete the assignment
+#-----------------------------------------------------------*/
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from mlxtend.frequent_patterns import apriori, association_rules
+
+#Use the command: "pip install mlxtend" on your terminal to install the mlxtend library
+
+#read the dataset using pandas
+df = pd.read_csv('retail_dataset.csv', sep=',')
+
+#find the unique items all over the data an store them in the set below
+itemset = set()
+for i in range(0, len(df.columns)):
+    items = (df[str(i)].unique())
+    itemset = itemset.union(set(items))
+
+#remove nan (empty) values by using:
+itemset.remove(np.nan)
+
+#To make use of the apriori module given by mlxtend library, we need to convert the dataset accordingly. Apriori module requires a
+# dataframe that has either 0 and 1 or True and False as data.
+#Example:
+
+#Bread Wine Eggs
+#1     0    1
+#0     1    1
+#1     1    1
+
+#To do that, create a dictionary (labels) for each transaction, store the corresponding values for each item (e.g., {'Bread': 0, 'Milk': 1}) in that transaction,
+#and when is completed, append the dictionary to the list encoded_vals below (this is done for each transaction)
+#-->add your python code below
+
+encoded_vals = []
+#key vals bread : 0, wine : 1, eggs : 2, meat : 3, cheese : 4, pencil : 5, diaper : 6, bagel : 7, Milk : 8
+for index, row in df.iterrows():
+    for i in range(0, 8):
+        if (row[i] == 'Bread'):
+            val[0] = 0
+            valCount[0]+= 1
+        if (row[i] == 'Wine'):
+            val[1] = 1
+            valCount[1]+= 1
+        if (row[i] == 'Eggs'):
+            val[2] = 1
+            valCount[2]+= 1
+        if (row[i] == 'Meat'):
+            val[3] = 1
+            valCount[3]+= 1
+        if (row[i] == 'Cheese'):
+            val[4] = 4
+            valCount[4]+= 1
+        if (row[i] == 'Pencil'):
+            val[5] = 1
+            valCount[5]+= 1
+        if (row[i] == 'Diaper'):
+            val[6] = 1
+            valCount[6]+= 1
+        if (row[i] == 'Bagel'):
+            val[7] = 1
+            valCount[7]+= 1
+        if (row[i] == 'Milk'):
+            val[8] = 1
+            valCount[8]+= 1
+
+    labels = {'Bread' : val[0], 'Wine' : val[1], 'Eggs': val[2], 'Meat' : val[3], 'Cheese': val[4],'Pencil': val[5],
+              'Diaper': val[6], 'Bagel': val[7], 'Milk' : val[8]}
+
+    encoded_vals.append(labels)
+
+#adding the populated list with multiple dictionaries to a data frame
+ohe_df = pd.DataFrame(encoded_vals)
+
+#calling the apriori algorithm informing some parameters
+freq_items = apriori(ohe_df, min_support=0.2, use_colnames=True, verbose=1)
+rules = association_rules(freq_items, metric="confidence", min_threshold=0.6)
+
+#iterate the rules data frame and print the apriori algorithm results by using the following format:
+
+#Meat, Cheese -> Eggs
+#Support: 0.21587301587301588
+#Confidence: 0.6666666666666666
+#Prior: 0.4380952380952381
+#Gain in Confidence: 52.17391304347825
+#-->add your python code below
+
+#To calculate the prior and gain in confidence, find in how many transactions the consequent of the rule appears (the supporCount below). Then,
+#use the gain formula provided right after.
+#prior = suportCount/len(encoded_vals) -> encoded_vals is the number of transactions
+#print("Gain in Confidence: " + str(100*(rule_confidence-prior)/prior))
+#-->add your python code below
+
+#Finally, plot support x confidence
+plt.scatter(rules['support'], rules['confidence'], alpha=0.5)
+plt.xlabel('support')
+plt.ylabel('confidence')
+plt.title('Support vs Confidence')
+plt.show()
